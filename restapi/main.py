6aded8a2b7659900f9bb6d4eabc070cacb5a0b6a
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-07-30 07:07:40 +0000

    Updated to async

--- restapi/main.py
+++ restapi/main.py
@@ -7,13 +7,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id):
+    return {"item_id": item_id}
 
 if __name__ == "__main__":
     app.run()
